952412430c94c79950475d54387a3941bcefa933
Branden Barber <bbarber@nebraskaglobal.com>
2016-05-31 11:38:51 -0500

    Log the host so I can filter later. Remove s3 code

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var express = require('express');
-var aws = require('aws-sdk');
 var cheerio = require('cheerio');
 var request = require('request');
 var app = express();
 
-var AWS_ACCESS_KEY_ID = process.env.s3_key;
-var AWS_SECRET_ACCESS_KEY = process.env.s3_secret;
 
 app.get('/', function (req, res) {
     
     console.log("request /");
+    console.log("Host:" + req.get('host'));
     
     var url = 'https://nationalbikechallenge.org/workplace/4511';
     request(url, function (error, response, html) {
@@ -47,53 +45,20 @@ app.get('/', function (req, res) {
             }
 
             var body = list.join('\r\n');
-
             console.log("parsed body");
+                        
+            var params = '?token=' + process.env.token + '&channel=%23' + process.env.channel;
+            var options = {
+                url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
+                body: "```" + body + "```"
+            };
             
-            var s3 = new aws.S3({ params: { Bucket: process.env.s3_bucket, Key: 'nbc.txt' } });
-
-            s3.getObject({
-                Bucket: process.env.s3_bucket,
-                Key: 'nbc.txt',
-                ResponseContentType: 'text/plain'
-            }, function (err, data) {
+            console.log("Before slack post");
+            request.post(options);
                 
-                console.log("success s3 read");
-                console.log(data);
-                console.log(err);
-                console.log(data.Body);
-                
-                // We get back a byte[] from s3, convert to string
-                var buf = new Buffer(data.Body.length);
-                for (var i = 0; i < data.Body.length; i++) {
-                    buf[i] = data.Body[i];
-                } 
-                var last = buf.toString('utf8');
-
-                console.log("before upload");
-
-                if (last !== body) {
-                    s3.upload({ Body: body, ACL: 'public-read', ContentType: 'text/plain' }, function () {
-                        if (err) {
-                            return console.log(err);
-                        }
-                        
-                        console.log("success upload s3");
-                        
-                        var params = '?token=' + process.env.token + '&channel=%23' + process.env.channel;
-                        var options = {
-                            url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
-                            body: "```" + body + "```"
-                        };
-                        
-                        console.log("Before slack post");
-                        request.post(options);
-                    });
-                }
-                
-                res.send('<pre>' + body + '</pre>');
-            });
-        }
+            res.send('<pre>' + body + '</pre>');
+        });
+    }
     })
 })
 
