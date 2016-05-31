0b336b497ac04e82f6c98390fa8eae27cbc81a9e
Branden Barber <bbarber@nebraskaglobal.com>
2016-05-31 11:42:02 -0500

    Fix braces

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ var request = require('request');
 var app = express();
 
 
-app.get('/', function (req, res) {
-    
+app.get('/', function(req, res) {
+
     console.log("request /");
     console.log("Host:" + req.get('host'));
-    
+
     var url = 'https://nationalbikechallenge.org/workplace/4511';
-    request(url, function (error, response, html) {
+    request(url, function(error, response, html) {
         if (!error) {
-            
+
             console.log("success on request");
             var $ = cheerio.load(html);
-            
-            var names = $('.-user .-name .title').map(function (i, a) {
+
+            var names = $('.-user .-name .title').map(function(i, a) {
                 return a.children[0].data;
             });
 
@@ -27,15 +27,15 @@ app.get('/', function (req, res) {
             }
 
             // Pad names so scores right align
-            names = names.map(function (i, a) {
+            names = names.map(function(i, a) {
                 return a + Array(maxLength - a.length + 1).join(' ');
             });
 
-            var scores = $('.-leader-table-desktop .points-bar>span').map(function (i, a) {
+            var scores = $('.-leader-table-desktop .points-bar>span').map(function(i, a) {
                 return a.children[0].data;
             });
 
-            var arr = scores.map(function (i, score) {
+            var arr = scores.map(function(i, score) {
                 return names[i] + ' - ' + score;
             });
 
@@ -46,19 +46,18 @@ app.get('/', function (req, res) {
 
             var body = list.join('\r\n');
             console.log("parsed body");
-                        
+
             var params = '?token=' + process.env.token + '&channel=%23' + process.env.channel;
             var options = {
                 url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
                 body: "```" + body + "```"
             };
-            
+
             console.log("Before slack post");
             request.post(options);
-                
+
             res.send('<pre>' + body + '</pre>');
-        });
-    }
+        }
     })
 })
 
